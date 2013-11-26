ca48af1e0f3689c1ce5b97bc7495e7cf56c6981e
Ken Koch <kenneth.koch@fmr.com>
2013-11-26 10:15:32 -0500

    moved HEAP and X into ENV

--- main.js
+++ main.js
@@ -19,8 +19,7 @@ var Compiler = require("./lib/compilers");
 /**
  * Create the X store and HEAP.
  **/
-var X = new Structures.X();
-var HEAP = new Structures.Heap();
+var ENV = new Structures.Env();
 
 /**
  * Parse and Flatten the Query: 
@@ -28,18 +27,18 @@ var HEAP = new Structures.Heap();
  *
  * Currently no code for this step :-(
  **/
-X.set(1, new CompleteStructure("p",3,[new StoreRef(X,2), new StoreRef(X,3), new StoreRef(X,4)]));
-X.set(2, new Variable("Z", new StoreRef(X,2)));
-X.set(3, new CompleteStructure("h", 2, [new StoreRef(X,2), new StoreRef(X,5)]));
-X.set(4, new CompleteStructure("f", 1, [new StoreRef(X,5)]));
-X.set(5, new Variable("W", new StoreRef(X,5)));
+ENV.X().set(1, new CompleteStructure("p",3,[new StoreRef(ENV.X(),2), new StoreRef(ENV.X(),3), new StoreRef(ENV.X(),4)]));
+ENV.X().set(2, new Variable("Z", new StoreRef(ENV.X(),2)));
+ENV.X().set(3, new CompleteStructure("h", 2, [new StoreRef(ENV.X(),2), new StoreRef(ENV.X(),5)]));
+ENV.X().set(4, new CompleteStructure("f", 1, [new StoreRef(ENV.X(),5)]));
+ENV.X().set(5, new Variable("W", new StoreRef(ENV.X(),5)));
 var flattened = [ 3, 4, 1 ];
 
 /**
  * Compile the argument registers into query instructions
  **/
-var queryInstructions = Compiler.CompileLoadedQuery(X, flattened);
-console.log(X);
+var queryInstructions = Compiler.CompileLoadedQuery(ENV.X(), flattened);
+console.log(ENV.X());
 console.log("Query Instructions:");
 console.log(queryInstructions);
 
@@ -49,20 +48,20 @@ console.log(queryInstructions);
  *
  * Currently no code for this step :-( 
  **/
-X.set(1, new CompleteStructure("p",3,[new StoreRef(X,2), new StoreRef(X,3), new StoreRef(X,4)]));
-X.set(2, new CompleteStructure("f",1,[new StoreRef(X,5)]));
-X.set(3, new CompleteStructure("h",2,[new StoreRef(X,4), new StoreRef(X,6)]));
-X.set(4, new Variable("Y", new StoreRef(X,4)));
-X.set(5, new Variable("X", new StoreRef(X,5)));
-X.set(6, new CompleteStructure("f",1,[new StoreRef(X,7)]));
-X.set(7, new CompleteStructure("a",0,[]));
+ENV.X().set(1, new CompleteStructure("p",3,[new StoreRef(ENV.X(),2), new StoreRef(ENV.X(),3), new StoreRef(ENV.X(),4)]));
+ENV.X().set(2, new CompleteStructure("f",1,[new StoreRef(ENV.X(),5)]));
+ENV.X().set(3, new CompleteStructure("h",2,[new StoreRef(ENV.X(),4), new StoreRef(ENV.X(),6)]));
+ENV.X().set(4, new Variable("Y", new StoreRef(ENV.X(),4)));
+ENV.X().set(5, new Variable("X", new StoreRef(ENV.X(),5)));
+ENV.X().set(6, new CompleteStructure("f",1,[new StoreRef(ENV.X(),7)]));
+ENV.X().set(7, new CompleteStructure("a",0,[]));
 flattened = [1,2,3,6,7];
 
 /**
  * Compile the argument registers into program instructions
  **/
-var programInstructions = Compiler.CompileLoadedProgram(X, flattened);
-console.log(X);
+var programInstructions = Compiler.CompileLoadedProgram(ENV.X(), flattened);
+console.log(ENV.X());
 console.log("Program Instructions:");
 console.log(programInstructions);
 
@@ -71,16 +70,16 @@ console.log(programInstructions);
  * Begin the evaluation by evaluating the QUERY.
  **/
 console.log("Evaluate Query");
-Instructions.evaluateInstructions(queryInstructions, HEAP, X);
-console.log(X);
-console.log(HEAP);
+Instructions.evaluateInstructions(queryInstructions, ENV);
+console.log(ENV.X());
+console.log(ENV.HEAP());
 
 console.log("Evaluate Program");
 try {
-	Instructions.evaluateInstructions(programInstructions, HEAP, X);
+	Instructions.evaluateInstructions(programInstructions, ENV);
 } catch(e) { console.log("[ERROR] " + e); }
-console.log(X);
-console.log(HEAP);
+console.log(ENV.X());
+console.log(ENV.HEAP());
 
 
 
