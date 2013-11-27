7c0a1d83085bde4d8618f823df4d17ca404dcd10
Ken Koch <kenneth.koch@fmr.com>
2013-11-27 10:16:26 -0500

    started building tests, added some other examples to main js

--- main.js
+++ main.js
@@ -17,84 +17,283 @@ var Instructions = require("./lib/instructions");
  **/
 var Compiler = require("./lib/compilers");
 
-/**
- * Create the X store and HEAP.
- **/
-var ENV = new Structures.Env();
+var DEBUG = false;
+var exampleNames = {
+	"main":WamBookExample,
+	"2var":UnifyTwoVariables,
+	"fail1":Failure1
+};
 
 /**
- * Parse and Flatten the Query: 
- * 		?- p(Z, h(Z,W), f(W)).
- *
- * Currently no code for this step :-(
+ * Check the args
  **/
-ENV.X().set(1, new CompleteStructure("p",3,[new StoreRef(ENV.X(),2), new StoreRef(ENV.X(),3), new StoreRef(ENV.X(),4)]));
-ENV.X().set(2, new Variable("Z", new StoreRef(ENV.X(),2)));
-ENV.X().set(3, new CompleteStructure("h", 2, [new StoreRef(ENV.X(),2), new StoreRef(ENV.X(),5)]));
-ENV.X().set(4, new CompleteStructure("f", 1, [new StoreRef(ENV.X(),5)]));
-ENV.X().set(5, new Variable("W", new StoreRef(ENV.X(),5)));
-var flattened = [ 3, 4, 1 ];
+if(process.argv.length === 2) {
+	printUsage();
+} else if(process.argv.length >= 3) {
+	if(exampleNames[process.argv[2]]) {
+		exampleNames[process.argv[2]](process.argv[3] === "debug");
+	} else {
+		console.log("Program "+process.argv[2]+" not found.");
+		printUsage();
+		process.exit();
+	}
+}
+
+function printUsage() {
+	console.log("Usage: node main.js <example> [debug]");
+	console.log("Current Examples:");
+	for (var i = Object.keys(exampleNames).length - 1; i >= 0; i--) {
+		console.log("\t" + Object.keys(exampleNames)[i]);
+	};
+	process.exit();
+}
 
 /**
- * Compile the argument registers into query instructions
+ * Try unifying terms that dont unify.
+ * Query: 			p(A,f(b)).
+ * Program: 		p(b, f(c)).
+ * Expected Result: failure
  **/
-var queryInstructions = Compiler.CompileLoadedQuery(ENV.X(), flattened);
-console.log(ENV.X());
-console.log("Query Instructions:");
-console.log(queryInstructions);
+function Failure1(DEBUG) {
+	/**
+	 * Create the X store and HEAP.
+	 **/
+	var ENV = new Structures.Env();
+
+	/**
+	 * Parse and Flatten the Query: 
+	 * 		?- p(A,f(b)).
+	 *
+	 * Currently no code for this step :-(
+	 **/
+	ENV.X().set(1, new CompleteStructure("p",2,[new StoreRef(ENV.X(),2), new StoreRef(ENV.X(), 3)]));
+	ENV.X().set(2, new Variable("A", new StoreRef(ENV.X(),2)));
+	ENV.X().set(3, new CompleteStructure("f",1,[new StoreRef(ENV.X(), 4)]));
+	ENV.X().set(4, new CompleteStructure("b",0,[]));
+	var flattened = [ 4, 3, 1 ];
+
+	/**
+	 * Compile the argument registers into query instructions
+	 **/
+	var queryInstructions = Compiler.CompileLoadedQuery(ENV.X(), flattened);
+	console.log(ENV.X());
+	console.log("Query Instructions:");
+	console.log(queryInstructions);
+
+	/**
+	 * Compile and Flatten the Program:
+	 *		p(b, f(c)).
+	 *
+	 * Currently no code for this step :-( 
+	 **/
+	ENV.X().set(1, new CompleteStructure("p",2,[new StoreRef(ENV.X(),2), new StoreRef(ENV.X(), 3)]));
+	ENV.X().set(2, new CompleteStructure("b",0,[]));
+	ENV.X().set(3, new CompleteStructure("f",1,[new StoreRef(ENV.X(), 4)]));
+	ENV.X().set(4, new CompleteStructure("c",0,[]));
+	var flattened = [ 1,2,3,4 ];
+
+	/**
+	 * Compile the argument registers into program instructions
+	 **/
+	var programInstructions = Compiler.CompileLoadedProgram(ENV.X(), flattened);
+	console.log(ENV.X());
+	console.log("Program Instructions:");
+	console.log(programInstructions);
+
+	/**
+	 * Begin the evaluation by evaluating the QUERY.
+	 **/
+	console.log("Evaluate Query");
+	Instructions.evaluateInstructions(queryInstructions, ENV, DEBUG);
+	console.log(ENV.X());
+	console.log(ENV.HEAP());
+
+	console.log("Evaluate Program");
+	try {
+		Instructions.evaluateInstructions(programInstructions, ENV, DEBUG);
+	} catch(e) { console.log("[ERROR] " + e); process.exit(); }
+	console.log(ENV.X());
+	console.log(ENV.HEAP());
+
+
+	/**
+	 * Try to rebuild the term.
+	 **/
+	// Need to hardcode the heap address where the entry point ends up :-(
+	var result = Utils.rebuildTermFromHeap(new StoreRef(ENV.HEAP(), 1));
+
+	console.log("\n\nResults: ");
+	console.log("Query Term:\t p(Z, h(Z,W), f(W)).");
+	console.log("Query Term:\t p(f(X), h(Y, f(a)), Y).");
+
+	// The correct answer should be: p( _H1 )
+	console.log("MGU:\t\t " + result.inspect() + ".");
+}
 
 /**
- * Compile and Flatten the Program:
- *		p(f(X), h(Y, f(a)), Y).
- *
- * Currently no code for this step :-( 
+ * Try a basic unification of two variables.
+ * Query: 			p(A).
+ * Program: 		p(Z).
+ * Expected Result: P(_H1)
  **/
-ENV.X().set(1, new CompleteStructure("p",3,[new StoreRef(ENV.X(),2), new StoreRef(ENV.X(),3), new StoreRef(ENV.X(),4)]));
-ENV.X().set(2, new CompleteStructure("f",1,[new StoreRef(ENV.X(),5)]));
-ENV.X().set(3, new CompleteStructure("h",2,[new StoreRef(ENV.X(),4), new StoreRef(ENV.X(),6)]));
-ENV.X().set(4, new Variable("Y", new StoreRef(ENV.X(),4)));
-ENV.X().set(5, new Variable("X", new StoreRef(ENV.X(),5)));
-ENV.X().set(6, new CompleteStructure("f",1,[new StoreRef(ENV.X(),7)]));
-ENV.X().set(7, new CompleteStructure("a",0,[]));
-flattened = [1,2,3,6,7];
+function UnifyTwoVariables(DEBUG) {
+	/**
+	 * Create the X store and HEAP.
+	 **/
+	var ENV = new Structures.Env();
+
+	/**
+	 * Parse and Flatten the Query: 
+	 * 		?- p(A)
+	 *
+	 * Currently no code for this step :-(
+	 **/
+	ENV.X().set(1, new CompleteStructure("p",1,[new StoreRef(ENV.X(),2)]));
+	ENV.X().set(2, new Variable("A", new StoreRef(ENV.X(),2)));
+	var flattened = [ 1 ];
+
+	/**
+	 * Compile the argument registers into query instructions
+	 **/
+	var queryInstructions = Compiler.CompileLoadedQuery(ENV.X(), flattened);
+	console.log(ENV.X());
+	console.log("Query Instructions:");
+	console.log(queryInstructions);
+
+	/**
+	 * Compile and Flatten the Program:
+	 *		p(Z).
+	 *
+	 * Currently no code for this step :-( 
+	 **/
+	ENV.X().set(1, new CompleteStructure("p",1,[new StoreRef(ENV.X(),2)]));
+	ENV.X().set(2, new Variable("Z", new StoreRef(ENV.X(),2)));
+	flattened = [1];
+
+	/**
+	 * Compile the argument registers into program instructions
+	 **/
+	var programInstructions = Compiler.CompileLoadedProgram(ENV.X(), flattened);
+	console.log(ENV.X());
+	console.log("Program Instructions:");
+	console.log(programInstructions);
+
+	/**
+	 * Begin the evaluation by evaluating the QUERY.
+	 **/
+	console.log("Evaluate Query");
+	Instructions.evaluateInstructions(queryInstructions, ENV, DEBUG);
+	console.log(ENV.X());
+	console.log(ENV.HEAP());
+
+	console.log("Evaluate Program");
+	try {
+		Instructions.evaluateInstructions(programInstructions, ENV, DEBUG);
+	} catch(e) { console.log("[ERROR] " + e); }
+	console.log(ENV.X());
+	console.log(ENV.HEAP());
+
+
+	/**
+	 * Try to rebuild the term.
+	 **/
+	// Need to hardcode the heap address where the entry point ends up :-(
+	var result = Utils.rebuildTermFromHeap(new StoreRef(ENV.HEAP(), 1));
+
+	console.log("\n\nResults: ");
+	console.log("Query Term:\t p(Z, h(Z,W), f(W)).");
+	console.log("Query Term:\t p(f(X), h(Y, f(a)), Y).");
+
+	// The correct answer should be: p( _H1 )
+	console.log("MGU:\t\t " + result.inspect() + ".");
+}
+
 
 /**
- * Compile the argument registers into program instructions
+ * The wam book example uses:
+ * Query: 			p(Z, h(Z,W), f(W)).
+ * Program: 		p(f(X), h(Y, f(a)), Y).
+ * Expected Result: p(f(f(a)),h(f(f(a)),f(a)),f(f(a))).
  **/
-var programInstructions = Compiler.CompileLoadedProgram(ENV.X(), flattened);
-console.log(ENV.X());
-console.log("Program Instructions:");
-console.log(programInstructions);
+function WamBookExample(DEBUG) {
+	/**
+	 * Create the X store and HEAP.
+	 **/
+	var ENV = new Structures.Env();
+
+	/**
+	 * Parse and Flatten the Query: 
+	 * 		?- p(Z, h(Z,W), f(W)).
+	 *
+	 * Currently no code for this step :-(
+	 **/
+	ENV.X().set(1, new CompleteStructure("p",3,[new StoreRef(ENV.X(),2), new StoreRef(ENV.X(),3), new StoreRef(ENV.X(),4)]));
+	ENV.X().set(2, new Variable("Z", new StoreRef(ENV.X(),2)));
+	ENV.X().set(3, new CompleteStructure("h", 2, [new StoreRef(ENV.X(),2), new StoreRef(ENV.X(),5)]));
+	ENV.X().set(4, new CompleteStructure("f", 1, [new StoreRef(ENV.X(),5)]));
+	ENV.X().set(5, new Variable("W", new StoreRef(ENV.X(),5)));
+	var flattened = [ 3, 4, 1 ];
+
+	/**
+	 * Compile the argument registers into query instructions
+	 **/
+	var queryInstructions = Compiler.CompileLoadedQuery(ENV.X(), flattened);
+	console.log(ENV.X());
+	console.log("Query Instructions:");
+	console.log(queryInstructions);
+
+	/**
+	 * Compile and Flatten the Program:
+	 *		p(f(X), h(Y, f(a)), Y).
+	 *
+	 * Currently no code for this step :-( 
+	 **/
+	ENV.X().set(1, new CompleteStructure("p",3,[new StoreRef(ENV.X(),2), new StoreRef(ENV.X(),3), new StoreRef(ENV.X(),4)]));
+	ENV.X().set(2, new CompleteStructure("f",1,[new StoreRef(ENV.X(),5)]));
+	ENV.X().set(3, new CompleteStructure("h",2,[new StoreRef(ENV.X(),4), new StoreRef(ENV.X(),6)]));
+	ENV.X().set(4, new Variable("Y", new StoreRef(ENV.X(),4)));
+	ENV.X().set(5, new Variable("X", new StoreRef(ENV.X(),5)));
+	ENV.X().set(6, new CompleteStructure("f",1,[new StoreRef(ENV.X(),7)]));
+	ENV.X().set(7, new CompleteStructure("a",0,[]));
+	flattened = [1,2,3,6,7];
+
+	/**
+	 * Compile the argument registers into program instructions
+	 **/
+	var programInstructions = Compiler.CompileLoadedProgram(ENV.X(), flattened);
+	console.log(ENV.X());
+	console.log("Program Instructions:");
+	console.log(programInstructions);
+
+	/**
+	 * Begin the evaluation by evaluating the QUERY.
+	 **/
+	console.log("Evaluate Query");
+	Instructions.evaluateInstructions(queryInstructions, ENV, DEBUG);
+	console.log(ENV.X());
+	console.log(ENV.HEAP());
+
+	console.log("Evaluate Program");
+	try {
+		Instructions.evaluateInstructions(programInstructions, ENV, DEBUG);
+	} catch(e) { console.log("[ERROR] " + e); }
+	console.log(ENV.X());
+	console.log(ENV.HEAP());
 
 
-/**
- * Begin the evaluation by evaluating the QUERY.
- **/
-console.log("Evaluate Query");
-Instructions.evaluateInstructions(queryInstructions, ENV);
-console.log(ENV.X());
-console.log(ENV.HEAP());
+	/**
+	 * Try to rebuild the term.
+	 **/
+	// Need to hardcode the heap address where the entry point ends up :-(
+	var result = Utils.rebuildTermFromHeap(new StoreRef(ENV.HEAP(), 8));
 
-console.log("Evaluate Program");
-try {
-	Instructions.evaluateInstructions(programInstructions, ENV);
-} catch(e) { console.log("[ERROR] " + e); }
-console.log(ENV.X());
-console.log(ENV.HEAP());
+	console.log("\n\nResults: ");
+	console.log("Query Term:\t p(Z, h(Z,W), f(W)).");
+	console.log("Query Term:\t p(f(X), h(Y, f(a)), Y).");
 
-
-/**
- * Try to rebuild the term.
- **/
-// Need to hardcode the heap address where the entry point ends up :-(
-var result = Utils.rebuildTermFromHeap(new StoreRef(ENV.HEAP(), 8));
-
-console.log("\n\nResults: ");
-console.log("Query Term:\t p(Z, h(Z,W), f(W)).");
-console.log("Query Term:\t p(f(X), h(Y, f(a)), Y).");
-
-// The correct answer should be: p( f(f(a)), h( f(f(a)),f(a) ), f(f(a)) )
-console.log("MGU:\t\t " + result.inspect() + ".");
+	// The correct answer should be: p( f(f(a)), h( f(f(a)),f(a) ), f(f(a)) )
+	console.log("MGU:\t\t " + result.inspect() + ".");
+}
 
 
 
---
+++ tests/compilation_tests.js
@@ -0,0 +1,129 @@
+var Structures          = require("../lib/structures");
+var StoreRef            = Structures.StoreRef;
+var CompleteStructure   = Structures.CompleteStructure;
+var Variable   			= Structures.Variable;
+var Utils 				= require("../lib/utils");
+var Instructions 		= require("../lib/instructions");
+var Compiler 			= require("../lib/compilers");
+
+/**
+ * A Test Suite for The L0 WAM Compilation.
+ **/
+describe("Query Compilation", function(){
+	var ENV = null;
+
+	beforeEach(function(){
+		ENV = new Structures.Env();	
+	});
+
+	/**
+	 * The example query from the wambook is: 
+	 *		?- p(Z, h(Z,W), f(W)).
+	 *
+	 * This test hard codes the parsed, flattened version and pipes it into compile.
+	 * It then checks the resulting instructions against the known L0 WAM Instructions.
+	 **/
+	it("should compile the WAMBook example query", function(){
+		ENV.X().set(1, new CompleteStructure("p",3,[new StoreRef(ENV.X(),2), new StoreRef(ENV.X(),3), new StoreRef(ENV.X(),4)]));
+		ENV.X().set(2, new Variable("Z", new StoreRef(ENV.X(),2)));
+		ENV.X().set(3, new CompleteStructure("h", 2, [new StoreRef(ENV.X(),2), new StoreRef(ENV.X(),5)]));
+		ENV.X().set(4, new CompleteStructure("f", 1, [new StoreRef(ENV.X(),5)]));
+		ENV.X().set(5, new Variable("W", new StoreRef(ENV.X(),5)));
+		var flattened = [ 3, 4, 1 ];
+		var queryInstructions = Compiler.CompileLoadedQuery(ENV.X(), flattened);
+
+		var expectedInstructions = [
+			new Instructions.put_structure("h", 2, new StoreRef(ENV.X(), 3)), 	// put_structure h/2 X[3],
+			new Instructions.set_variable(new StoreRef(ENV.X(), 2)), 			// set_variable X[2],
+			new Instructions.set_variable(new StoreRef(ENV.X(), 5)), 			// set_variable X[5],
+			new Instructions.put_structure("f", 1, new StoreRef(ENV.X(), 4)), 	// put_structure f/1 X[4],
+			new Instructions.set_value(new StoreRef(ENV.X(), 5)), 				// set_value X[5],
+			new Instructions.put_structure("p", 3, new StoreRef(ENV.X(), 1)), 	// put_structure p/3 X[1],
+			new Instructions.set_value(new StoreRef(ENV.X(), 2)), 				// set_value X[2],
+			new Instructions.set_value(new StoreRef(ENV.X(), 3)), 				// set_value X[3],
+			new Instructions.set_value(new StoreRef(ENV.X(), 4)) 				// set_value X[4]
+		];
+
+		compareInstructionLists(expectedInstructions, queryInstructions);
+	});
+
+});
+
+describe("Program Compilation", function(){
+	var ENV = null;
+
+	beforeEach(function(){
+		ENV = new Structures.Env();	
+	});
+
+	/**
+	 * The example program from the wambook is: 
+	 *		p(f(X), h(Y, f(a)), Y).
+	 *
+	 * This test hard codes the parsed, flattened version and pipes it into compile.
+	 * It then checks the resulting instructions against the known L0 WAM Instructions.
+	 **/
+	it("should compile the WAMBook example program", function(){
+		ENV.X().set(1, new CompleteStructure("p",3,[new StoreRef(ENV.X(),2), new StoreRef(ENV.X(),3), new StoreRef(ENV.X(),4)]));
+		ENV.X().set(2, new CompleteStructure("f",1,[new StoreRef(ENV.X(),5)]));
+		ENV.X().set(3, new CompleteStructure("h",2,[new StoreRef(ENV.X(),4), new StoreRef(ENV.X(),6)]));
+		ENV.X().set(4, new Variable("Y", new StoreRef(ENV.X(),4)));
+		ENV.X().set(5, new Variable("X", new StoreRef(ENV.X(),5)));
+		ENV.X().set(6, new CompleteStructure("f",1,[new StoreRef(ENV.X(),7)]));
+		ENV.X().set(7, new CompleteStructure("a",0,[]));
+		flattened = [1,2,3,6,7];
+		var programInstructions = Compiler.CompileLoadedProgram(ENV.X(), flattened);
+
+		var expectedInstructions = [
+			new Instructions.get_structure("p", 3, new StoreRef(ENV.X(), 1)), 	// get_structure p/3 X[1]
+			new Instructions.unify_variable(new StoreRef(ENV.X(), 2)), 			// unify_variable X[2]
+			new Instructions.unify_variable(new StoreRef(ENV.X(), 3)), 			// unify_variable X[3]
+			new Instructions.unify_variable(new StoreRef(ENV.X(), 4)), 			// unify_variable X[4]
+			new Instructions.get_structure("f", 1, new StoreRef(ENV.X(), 2)), 	// get_structure f/1 X[2]
+			new Instructions.unify_variable(new StoreRef(ENV.X(), 5)), 			// unify_variable X[5]
+			new Instructions.get_structure("h", 2, new StoreRef(ENV.X(), 3)), 	// get_structure h/2 X[3]
+			new Instructions.unify_value(new StoreRef(ENV.X(), 4)), 			// unify_value X[4]
+			new Instructions.unify_variable(new StoreRef(ENV.X(), 6)), 			// unify_variable X[6]
+			new Instructions.get_structure("f", 1, new StoreRef(ENV.X(), 6)), 	// get_structure f/1 X[6]
+			new Instructions.unify_variable(new StoreRef(ENV.X(), 7)), 			// unify_variable X[7]
+			new Instructions.get_structure("a", 0, new StoreRef(ENV.X(), 7)) 	// get_structure f/1 X[7]
+		];
+
+		compareInstructionLists(expectedInstructions, programInstructions);
+	});
+});
+
+/**
+ * Compare two lists of instructions.
+ **/
+function compareInstructionLists(list_a, list_b) {
+	// Ensure the length is the same
+	list_a.length.should.equal(list_b.length);
+
+	// Ensure that each instruction is correct.
+	for (var i = list_a.length - 1; i >= 0; i--) {
+		var expectedInstruction = list_b[i];
+		var actualInstruction = list_a[i];
+		
+		// Make sure they're the same instruction
+		actualInstruction.constructor.should.equal(expectedInstruction.constructor);
+
+		// Make sure their params match
+		switch(actualInstruction.constructor) {
+			case Instructions.put_structure:
+			case Instructions.get_structure:
+				actualInstruction.symbol.should.equal(expectedInstruction.symbol);
+				actualInstruction.arity.should.equal(expectedInstruction.arity);
+			case Instructions.set_variable:
+			case Instructions.set_value:
+			case Instructions.unify_variable:
+			case Instructions.unify_value:
+				actualInstruction.reference.store.should.equal(expectedInstruction.reference.store);
+				actualInstruction.reference.index.should.equal(expectedInstruction.reference.index);
+				break ;
+			default:
+				// fail.
+				true.should.equal(false);
+		}
+	};
+}
